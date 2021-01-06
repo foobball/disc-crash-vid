1c34cce2e66542802b5854a39d5b141c6786d08c
foob <foobbal@gmail.com>
2021-01-06 11:59:33 -0500

    finished and working 😱

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const consola = require('consola');
+const { exec } = require("child_process");
 let files = [];
 
 function getFiles(callback) {
@@ -13,14 +14,52 @@ function getFiles(callback) {
 async function convert() {
     getFiles(() => {
         if (files.indexOf('ffmpeg.exe') == -1) {
-            consola.error('Could not find ffmpeg.exe');
+            consola.error('Could not find ffmpeg.exe\n    Download ffmpeg.exe from https://github.com/foobball/disc-crash-vid/raw/main/ffmpeg.exe');
             process.exit();
         }
 
+        if (files.indexOf('template.mp4') == -1) {
+            consola.error('Could not find template.mp4\n    Download template.mp4 from https://github.com/foobball/disc-crash-vid/raw/main/template.mp4');
+            process.exit();
+        }
 
+        let mp4file = 'main.mp4';
+        process.argv.forEach((arg) => {
+            if (arg.length >= 4) {
+                if (arg.substr(arg.length - 4, 4) == '.mp4') {
+                    mp4file = arg;
+                }
+            }
+        })
+
+        if (files.indexOf(mp4file) == -1) {
+            consola.error('Could not find ' + mp4file);
+            process.exit();
+        }
+
+        fs.writeFile('.config.temp', `file '${mp4file}'
+file 'template.mp4'
+inpoint 1000`, function(err) {
+            if (err) throw err;
+
+            // ffmpeg -y -f concat -i config.txt -c copy output.mp4
+            exec('ffmpeg -hide_banner -loglevel panic -y -f concat -i .config.temp -c copy output.mp4', (error, stdout, stderr) => {
+                if (error) {
+                    consola.error(error.message);
+                    return;
+                } else if (stderr) {
+                    consola.error(stderr);
+                    return;
+                } else {
+                    consola.success(`Successfully created glitched file "output.mp4"`);
+                }
+
+                fs.unlink('./.config.temp', () => {
+                    process.exit();
+                })
+            })
+        });
     });
 }
-
-console.log(process.argv);
 
 convert();
